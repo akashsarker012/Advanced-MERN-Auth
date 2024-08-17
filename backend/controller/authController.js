7823c8835f1e026712468e579c154b29c61f2f41
akashsarker012 <akashsarker.dev@gmail.com>
2024-08-17 16:46:45 +0600

    create a user

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,48 +1,51 @@
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcryptjs');
-const { TokenAndSetCookie } = require('../utils/TokenAndSetCookie');
-
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { TokenAndSetCookie } = require("../utils/TokenAndSetCookie");
 const prisma = new PrismaClient();
-
 const singUp = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        if (!name || !email || !password) {
-            return res.status(400).json({ message: 'All fields are required' });
-        }
-
-        const existingUser = await prisma.user.findUnique({ where: { email } });
-
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
-
-        const userData = {
-            name,
-            email,
-            password: hashedPassword,
-            verificationToken,
-            verificationExpires: Date.now() + 86400000 
-        };
-        const user = await prisma.user.create({ data: userData });
-
-        TokenAndSetCookie(res, user.id);
-        res.status(200).json({
-            success: true,
-            message: 'User created successfully',
-            user: {
-                ...user,
-                password: undefined
-            }
-        });
-
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-}
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+      verificationToken,
+      isVerified: false,
+      lastLoginAt: new Date(), // Convert to Date object
+      verificationExpires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours from now
+    };
+
+    const user = await prisma.user.create({ data: userData });
+    TokenAndSetCookie(res, user.id);
+
+    res.status(200).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 module.exports = { singUp };
--- backend/index.js
+++ backend/index.js
@@ -3,30 +3,11 @@ const express = require('express');
 const { connectDatabase } = require('./database/connectDb');
 require('dotenv').config();
 const apiRoutes = require('./routes/index');
-const prisma = new PrismaClient();
-
-
-
 const app = express();
 const port = 5000;
 connectDatabase()
 
 app.use(express.json());
-
-// app.post('/user', async (req, res) => {
-//   const { name, email } = req.body;
-
-//   try {
-//     const newUser = await prisma.user.create({
-//       data: { name, email },
-//     });
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     console.error('Error creating user:', error);
-//     res.status(500).send('An error occurred while creating the user');
-//   }
-// });
-
 app.use(apiRoutes)
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
